c1fedd692e50c86755c9f3e2420e44160f199f24
saitek <saitek300@gmail.com>
2023-02-09 09:29:57 -0800

    bug fix for forcast

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,62 @@
+
+var searchHistory =[];
 var searchBtn = document.getElementById("submit-button");
 var searchBarInput = document.querySelector(".searchbarInput");
-var name = document.querySelector('.name');
+var cityName = document.querySelector('.name');
 var temp = document.querySelector('.temp');
 var wind = document.querySelector('.wind');
 var humidity = document.querySelector('.humidity');
 var apiKey = "1fb41e48b657cf2ffc887ad1435cafe9"
-
+const cardsContainer = document.getElementById('cardsContainer')
 
 function getApi() {
-   var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchBarInput.value + "&appid=" + apiKey;
+    var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchBarInput.value + "&units=imperial&appid=" + apiKey;
 
-   fetch(requestUrl)
-   .then(function (response) {
-    if (response.ok){
-        return response.json()
-    }
-    return null
-})
-   .then(function(data){
-    console.log(data)
-   });
+    fetch(requestUrl)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+            return null
+        })
+        .then(function (data) {
+            console.log(data)
+            getFiveDays(data.coord.lat, data.coord.lon)
+            cityName.textContent = data.name
+            wind.textContent = data.wind.speed
+            temp.textContent = data.main.temp
+            humidity.textContent = data.main.humidity
+        });
 }
 
-searchBtn.addEventListener("click",getApi);
+function getFiveDays(lat, lon) {
+    const requestApi = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+
+    fetch(requestApi)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+            return null
+        })
+        .then(function (data) {
+            console.log(data)
+            for (let i = 0; i < data.list.length; i++) {
+                const el = data.list[i];
+                if (el.dt_txt.includes('06:00:00')) {
+
+
+                    const card = document.createElement('div')
+                    card.classList.add('card')
+                    card.innerHTML = `<div class = 'card-body'>Temp: ${el.main.temp} \nWind: ${el.wind.speed} \nHumidity: ${el.main.humidity}</div>`
+
+                    cardsContainer.append(card)
+                }
+            }
+        })
+
+}
+
+
+
+searchBtn.addEventListener("click", getApi);
